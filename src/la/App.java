9a2f8d4ef5374671b2f30a7d77eaea140e2c289c
EvAlGodGood <Gooodfile@mail.ru>
2020-04-04 21:30:45 +0800

    задание 2

--- src/la/App.java
+++ src/la/App.java
@@ -1,42 +1,42 @@
-//package la;
-//
-//import java.util.ArrayList;
-//
-//public class App {
-//    public static void main(String args[]) {
-//        MassGen<Integer> intMass = new MassGen<Integer>(0, 1, 2, 3, 4, 5); //не сообразил как изобразить буфер
-//        MassGen<String> strMass = new MassGen<String>("", "a", "b", "c", "d", "e");//поэтому [0] вместо него
-//        MassGen<Float> floMass = new MassGen<Float>(0f, 0.25f, 1.5f, 2.5f, 1.1f, 0.125f);
-//
-//        intMass.printMass();
-//        strMass.printMass();
-//        floMass.printMass();
-//
-//        intMass.swapEL(3, 4);
-//        strMass.swapEL(1, 5);
-//        floMass.swapEL(1, 4);
-//
-//        intMass.printMass();
-//        strMass.printMass();
-//        floMass.printMass();
-//
-//        System.out.println(intMass.getMassType());
-//        System.out.println(strMass.getMassType());
-//        System.out.println(floMass.getMassType());
-//
-//
-//        //ArrayList <getMassType()> arrayList = new ArrayList<>();
-//
-////        ArrayList<intMass.getMassType()> arrayList = new ArrayList<>();
-////    }
-////    public void genArrList(){
-////        String s = intMass.getMassType();
-////
-////        ArrayList <s> arrayList = new ArrayList<>();
-////    }
-//
-//
+package la;
+
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String args[]) {
+        MassGen<Integer> intMass = new MassGen<Integer>(0, 1, 2, 3, 4, 5); //не сообразил как изобразить буфер
+        MassGen<String> strMass = new MassGen<String>("", "a", "b", "c", "d", "e");//поэтому [0] вместо него
+        MassGen<Float> floMass = new MassGen<Float>(0f, 0.25f, 1.5f, 2.5f, 1.1f, 0.125f);
+
+        intMass.printMass();
+        strMass.printMass();
+        floMass.printMass();
+
+        intMass.swapEL(3, 4);
+        strMass.swapEL(1, 5);
+        floMass.swapEL(1, 4);
+
+        intMass.printMass();
+        strMass.printMass();
+        floMass.printMass();
+
+        System.out.println(intMass.getMassType());
+        System.out.println(strMass.getMassType());
+        System.out.println(floMass.getMassType());
+
+
+        //ArrayList <getMassType()> arrayList = new ArrayList<>();
+
+//        ArrayList<intMass.getMassType()> arrayList = new ArrayList<>();
 //    }
-//}
+//    public void genArrList(){
+//        String s = intMass.getMassType();
+//
+//        ArrayList <s> arrayList = new ArrayList<>();
+//    }
+
+
+    }
+}
 
 
--- src/la/MassGen.java
+++ src/la/MassGen.java
@@ -1,40 +1,40 @@
-//package la;
-//
-//import java.util.ArrayList;
-//
-//public class MassGen<T> {
-//    private T[] mass;
-//
-//    public MassGen(T... mass) {
-//        this.mass = mass;
-//    }
-//
-//    public void swapEL(int i, int j) { //меняем местами два конкретных элемента экземляра класса
-//        mass [0] = mass [i];
-//        mass [i] = mass [j];
-//        mass [j] = mass [0];
-//    }
-//
-//    public T getMass(int i) {
-//        return mass[i];
-//    }
-//
-//
-//    public void printMass() {//вывод массива в экземпляре класса
-//        for(int i=1;i<mass.length;i++){
-//            System.out.print(mass[i] +" ");
-//        }
-//        System.out.println();
-//    }
-//
-//    public T getMassType() { //возьмем тип данных в конкретном экземпляре класса
-//        return (T) mass[0].getClass().getSimpleName();
-//    }
-//
-//    //public void genArrList(){
-//
-//       // ArrayList <getMassType()> arrayList = new ArrayList<>();
-//
-//    //}
-//
-//}
+package la;
+
+import java.util.ArrayList;
+
+public class MassGen<T> {
+    private T[] mass;
+
+    public MassGen(T... mass) {
+        this.mass = mass;
+    }
+
+    public void swapEL(int i, int j) { //меняем местами два конкретных элемента экземляра класса
+        mass [0] = mass [i];
+        mass [i] = mass [j];
+        mass [j] = mass [0];
+    }
+
+    public T getMass(int i) {
+        return mass[i];
+    }
+
+
+    public void printMass() {//вывод массива в экземпляре класса
+        for(int i=1;i<mass.length;i++){
+            System.out.print(mass[i] +" ");
+        }
+        System.out.println();
+    }
+
+    public T getMassType() { //возьмем тип данных в конкретном экземпляре класса
+        return (T) mass[0].getClass().getSimpleName();
+    }
+
+    //public void genArrList(){
+
+       // ArrayList <getMassType()> arrayList = new ArrayList<>();
+
+    //}
+
+}
